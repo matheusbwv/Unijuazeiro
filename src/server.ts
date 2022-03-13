47e9bc7821819d7b6719bd7dd1f7a4fb726dd231
matheus wenes <matheus.wenes@gmail.com>
2022-03-13 02:44:37 -0300

    atividade

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import { parse } from "path";
+
 const prompt = require("prompt-sync")();
 const n1 = prompt("digite um número: ")
 const n2 = prompt("digite um número: ")
@@ -7,8 +9,8 @@ const media = ((parseInt(n1) + parseInt(n2) + parseInt(n3))/3)
 
 
 if (media < 6 ){
-    console.log("reprovado")
-}else if (media > 6){
-    console.log("aprovado") 
+    console.log("sua media foi " + media.toFixed(2) + " : reprovado")
+} else {
+    console.log("sua media foi " + media.toFixed(2) + " : aprovado") 
 }
 
